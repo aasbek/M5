b0e85dbbca7f180eaa9bf03cc6a47aeb531ee199
Åshild Bekken <aashilb@ntnu.no>
2019-04-10 17:18:13 +0200

    new repository

---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+public class Vehicle {
+	
+	public int number;
+	public Node startDepot;
+	public Vector<Integer> vehicleRoutes; 
+	
+	
+	public Vehicle() {
+		this.number = number;
+	}
+
+}
